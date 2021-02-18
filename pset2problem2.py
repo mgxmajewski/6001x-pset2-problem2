8ba8b9108ab9e8dc139a02c1e85ba5969dbd468d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-02-18 20:11:07 +0100

    Initial commit

---
+++ pset2problem2.py
@@ -0,0 +1,12 @@
+# Paying Debt Off in a Year
+
+# Calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months.
+# By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant
+# amount that will be paid each month.
+
+# Given input
+balance = 3329
+annualInterestRate = 0.2
+# Expected output "Lowest Payment: 310"
+
+
